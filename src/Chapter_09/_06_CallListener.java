16ac7fcd34a9ec3c4874e8a2cf1b36945688a3ca
Hyeokjin Yun <hjyun6130@gmail.com>
2023-11-15 15:26:46 +0900

    23.11.14update

---
+++ src/Chapter_09/_06_CallListener.java
@@ -0,0 +1,8 @@
+package Chapter_09;
+
+public class _06_CallListener implements _06_Button.OnClickListener{
+    @Override
+    public void onClick(){
+        System.out.println("Calling");
+    }
+}
---
+++ src/Chapter_09/_08_RemoteControl.java
@@ -0,0 +1,6 @@
+package Chapter_09;
+
+public interface _08_RemoteControl {
+    public void turnOn();
+    public void turnOff();
+}

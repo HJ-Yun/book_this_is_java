61a3ecb407e158b0990de878eadf31425eb573f8
Hyeokjin Yun <hjyun6130@gmail.com>
2023-11-24 19:13:57 +0900

    23.11.24update

---
+++ src/Chapter_10/_12_BalanceException.java
@@ -0,0 +1,9 @@
+package Chapter_10;
+
+public class _12_BalanceException extends Exception{
+    public _12_BalanceException(){
+    }
+    public _12_BalanceException(String message){
+        super(message);
+    }
+}

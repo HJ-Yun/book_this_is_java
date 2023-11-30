069efa8abb91e1e4fee714269efcfb5e71272d4c
Hyeokjin Yun <hjyun6130@gmail.com>
2023-11-30 14:33:27 +0900

    23.11.29update

---
+++ src/Chapter_11/_07_Counter.java
@@ -0,0 +1,14 @@
+package Chapter_11;
+
+public class _07_Counter {
+    public int no;
+
+    public _07_Counter(int no){
+        this.no = no;
+    }
+
+    @Override
+    protected void finalize() throws Throwable{
+        System.out.println(no + "..Finalize");
+    }
+}
---
+++ src/Chapter_11/_17_SystemEnvExample.java
@@ -0,0 +1,8 @@
+package Chapter_11;
+
+public class _17_SystemEnvExample {
+    public static void main(String[] args) {
+        String javaHome = System.getenv("JAVA_HOME");
+        System.out.println("JAVA_HOME : " + javaHome);
+    }
+}

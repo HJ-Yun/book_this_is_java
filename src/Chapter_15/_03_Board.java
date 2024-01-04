f5105d07be69cd2b66a133b0d5820554443d1ce4
Hyeokjin Yun <hjyun6130@gmail.com>
2024-01-04 17:51:10 +0900

    24.01.04update

---
+++ src/Chapter_15/_03_Board.java
@@ -0,0 +1,13 @@
+package Chapter_15;
+
+public class _03_Board {
+    String subject;
+    String content;
+    String writer;
+
+    public _03_Board(String subject, String content, String writer){
+        this.subject = subject;
+        this.content = content;
+        this.writer = writer;
+    }
+}

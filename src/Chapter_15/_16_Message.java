4fc34926cdc3553c740369980b55054419360ef3
Hyeokjin Yun <hjyun6130@gmail.com>
2024-01-05 17:07:22 +0900

    24.01.05update

---
+++ src/Chapter_15/_16_Message.java
@@ -0,0 +1,11 @@
+package Chapter_15;
+
+public class _16_Message {
+    public String command;
+    public String to;
+
+    public _16_Message(String command, String to){
+        this.command = command;
+        this.to = to;
+    }
+}

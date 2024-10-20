967314de879ccdfc1c50357f8d3b8636c4f159eb
HJ Yun <142900952+HJ-Yun@users.noreply.github.com>
2024-10-20 14:41:49 +0900

    Delete src/Chapter_12 directory

--- src/Chapter_12/_04_ThreadB.java
+++
@@ -1,9 +0,0 @@
-package chapter_12;
-
-public class _04_ThreadB extends Thread{
-    public void run(){
-        for (int i = 0; i < 2; i++){
-            System.out.println(getName() + " is printing.");
-        }
-    }
-}
--- src/Chapter_12/_07_ThreadStateExample.java
+++
@@ -1,9 +0,0 @@
-package chapter_12;
-
-public class _07_ThreadStateExample {
-    public static void main(String[] args) {
-        _07_StatePrintThread statePrintThread
-                = new _07_StatePrintThread(new _07_TargetThread());
-        statePrintThread.start();
-    }
-}

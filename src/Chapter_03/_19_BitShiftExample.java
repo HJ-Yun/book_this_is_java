e41f079f7e224f7a2122394ac37f2b509c48e907
HJ Yun <142900952+HJ-Yun@users.noreply.github.com>
2024-10-20 14:40:04 +0900

    Delete src/Chapter_03 directory

--- src/Chapter_03/_19_BitShiftExample.java
+++
@@ -1,9 +0,0 @@
-package chapter_03;
-
-public class _19_BitShiftExample {
-    public static void main(String[] args) {
-        System.out.println("1 << 3 = " + (1 << 3));
-        System.out.println("-8 >> 3 = " + (-8 >> 3));
-        System.out.println("-8 >>> 3 = " + (-8 >>> 3));
-    }
-}
--- src/Chapter_03/_21_ConditionOperationExample.java
+++
@@ -1,9 +0,0 @@
-package chapter_03;
-
-public class _21_ConditionOperationExample {
-    public static void main(String[] args) {
-        int score = 85;
-        char grade = score > 90 ? 'A' : (score > 80 ? 'B' : 'C');
-        System.out.println("Grade is " + grade);
-    }
-}

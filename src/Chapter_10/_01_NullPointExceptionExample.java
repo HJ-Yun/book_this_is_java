878a5df2be93a320aa3a43b5616eb43c0890f2b7
HJ Yun <142900952+HJ-Yun@users.noreply.github.com>
2024-10-20 14:41:26 +0900

    Delete src/Chapter_10 directory

--- src/Chapter_10/_01_NullPointExceptionExample.java
+++
@@ -1,8 +0,0 @@
-package chapter_10;
-
-public class _01_NullPointExceptionExample {
-    public static void main(String[] args) {
-        String data = null;
-        System.out.println(data.toString()); //NullPointException
-    }
-}
--- src/Chapter_10/_12_BalanceException.java
+++
@@ -1,9 +0,0 @@
-package chapter_10;
-
-public class _12_BalanceException extends Exception{
-    public _12_BalanceException(){
-    }
-    public _12_BalanceException(String message){
-        super(message);
-    }
-}

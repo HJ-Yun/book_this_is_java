b76a4d55b5a00b50cebc56ff58185e2e6df4bf0a
HJ Yun <142900952+HJ-Yun@users.noreply.github.com>
2024-10-20 14:46:03 +0900

    Delete src/Chapter_18 directory

--- src/Chapter_18/_19_ClassB.java
+++
@@ -1,7 +0,0 @@
-package chapter_18;
-
-import java.io.Serializable;
-
-public class _19_ClassB implements Serializable {
-    int filed1;
-}
--- src/Chapter_18/_21_Parent.java
+++
@@ -1,5 +0,0 @@
-package chapter_18;
-
-public class _21_Parent {
-    public String field1;
-}
